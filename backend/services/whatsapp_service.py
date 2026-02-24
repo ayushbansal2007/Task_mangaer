93645d1e0fd37589f107e159ea74ed677e7c20e1
ayushbansal2007 <a39169576@gmail.com>
2026-02-24 17:42:11 +0530

    Sensitive info moved to environment variables

--- backend/services/whatsapp_service.py
+++ backend/services/whatsapp_service.py
@@ -1,19 +1,21 @@
+import os
 from twilio.rest import Client
 
 def send_whatsapp_reminder(phone, task_title):
-    # Aapka real credentials
-    account_sid = 'AC8b1b6375f1151e045d04184e9dc366e6'
-    auth_token = 'beab51fb7bb02e5c749013a2776e80a0' 
+    # .env ya Render dashboard se values uthayega
+    account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+    auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+    from_number = os.environ.get('TWILIO_FROM_NUMBER')
     
     client = Client(account_sid, auth_token)
 
     try:
         message = client.messages.create(
-            from_='whatsapp:+14155238886', # Twilio Sandbox Number
-            body=f"🚀 *TASK REMINDER* 🚀\n\nBhai, aapka task *'{task_title}'* ka time ho gaya hai. Chalo shuru ho jao! we create a history lets do it",
+            from_=from_number,
+            body=f"🚀 *TASK REMINDER* 🚀\n\nBhai, aapka task *'{task_title}'* ka time ho gaya hai. Chalo shuru ho jao!",
             to=f"whatsapp:{phone}"
         )
-        print(f"✅ Success! Message sent to {phone}. SID: {message.sid}")
+        print(f"✅ Message Sent! SID: {message.sid}")
         return True
     except Exception as e:
         print(f"❌ Twilio Error: {e}")
